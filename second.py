b24a78be92b4db255d51b58a6ff8d57399cb6b69
Aditya Pinnika <106610442+adityapinnika2003@users.noreply.github.com>
2022-09-05 10:59:00 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pickle
+pickle_in=open("SalaryPrediction.pkl","rb")
+model=pickle.load(pickle_in)
+
+
+years=st.number_input("YEARS OF EXPERIENCE")
+if st.button("PREDICT"):  
+  salary=model.predict([[years]])
+  st.success(f'SALARY PREDICTION IS {salary}')
